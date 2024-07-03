2fba6de8cf0b0f34eabb8db46ad1510d9ac264e7
xynrgy <70825837+xynrgys@users.noreply.github.com>
2024-07-03 14:25:04 -0700

    Update CreatePostClient.tsx

--- app/create-post/CreatePostClient.tsx
+++ app/create-post/CreatePostClient.tsx
@@ -1,11 +1,17 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
 import CreatePost from '@/components/CreatePost'
+import { useSearchParams } from 'next/navigation'
 
 export default function CreatePostClient() {
   const searchParams = useSearchParams()
   const subredditId = searchParams.get('subredditId')
+  const subredditName = searchParams.get('subredditName')
 
-  return <CreatePost subredditId={subredditId || ''} />
+  return (
+    <CreatePost
+      subredditId={subredditId || ''}
+      subredditName={subredditName || ''}
+    />
+  )
 }
